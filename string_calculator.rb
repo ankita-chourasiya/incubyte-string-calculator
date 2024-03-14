406900025bb992958a8a331242548809896ac7f3
Ankita <ankitachourasiya11@gmail.com>
2024-03-15 01:36:28 +0530

    Support different delimiters.

--- string_calculator.rb
+++ string_calculator.rb
@@ -2,6 +2,18 @@ class StringCalculator
   def self.add(numbers)
     return 0 if numbers.empty?
 
-    numbers.split(/,|\n/).map(&:to_i).sum
+    numbers_array = if numbers.start_with?("//")
+                      change_delimiter(numbers)
+                    else
+                      numbers.split(/,|\n/)
+                    end
+
+    numbers_array.map(&:to_i).sum
+  end
+
+  def self.change_delimiter(numbers)
+    delimiter, numbers = numbers.split("\n", 2)
+    delimiter = delimiter[2..-1]
+    numbers.split(delimiter)
   end
 end
