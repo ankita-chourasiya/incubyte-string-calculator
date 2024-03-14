083c2533f41f9adc7df65f44df25e37a9a44774a
Ankita <ankitachourasiya11@gmail.com>
2024-03-15 01:09:07 +0530

    Handle new lines between numbers.

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -19,5 +19,11 @@ RSpec.describe StringCalculator do
         expect(StringCalculator.add("1,2")).to eq(3)
       end
     end
+
+    context "given '1\n2,3'" do
+      it "returns 6" do
+        expect(StringCalculator.add("1\n2,3")).to eq(6)
+      end
+    end
   end
 end
