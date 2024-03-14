9530fdda844bf4ca80c4afd494c3827cbcda0884
Ankita <ankitachourasiya11@gmail.com>
2024-03-15 00:30:07 +0530

    Simplest test case for empty string and for one & two numbers.

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -1,4 +1,23 @@
 require_relative '../string_calculator'
 
 RSpec.describe StringCalculator do
+  describe ".add" do
+    context "given an empty string" do
+      it "returns 0" do
+        expect(StringCalculator.add("")).to eq(0)
+      end
+    end
+
+    context "given '1'" do
+      it "returns 1" do
+        expect(StringCalculator.add("1")).to eq(1)
+      end
+    end
+
+    context "given '1,2'" do
+      it "returns 3" do
+        expect(StringCalculator.add("1,2")).to eq(3)
+      end
+    end
+  end
 end
